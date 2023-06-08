80736d8cef351c1faa5e0b05a6e7623e242e5339
Logan Hillyer <shivastranded@proton.me>
2023-06-08 11:58:31 +0700

    printavble characters

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 with open('key.txt', 'r') as key:
     key = key.read()
-
+# 65
+char_count = 94
+# def peek(x): print(list(x))
+# peek = lambda x: print(list(x))
 def numeric (string):
-    return map(lambda x: ord(x)-65, string)
+    return map(lambda x: ord(x)-32, string)
 
 def alphabetic (numbers):
-    result =  map(lambda x: chr(x+65), numbers)
+    result =  map(lambda x: chr(x+32), numbers)
+    # result = list(result)
     return ''.join(result)
 
 def encrypt(plaintext, key):
-    plaintext = numeric(plaintext)
-    key = numeric(key)
-    result = map(lambda a, b: (a+b)%26, plaintext, key)
+    plaintext = list( numeric(plaintext) )
+    key = list( numeric(key) )
+    print(plaintext)
+    print(key)
+    print(type(plaintext))
+    print(type(key))
+    result = map(lambda a, b: (a+b)%char_count, plaintext, key)
+    result = list(result)
     return alphabetic(result)
 
 def decrypt(ciphertext, key):
     ciphertext = numeric(ciphertext)
     key = numeric(key)
-    result = map(lambda a, b: (a-b)%26, ciphertext, key)
+    result = map(lambda a, b: (a-b)%char_count, ciphertext, key)
     return alphabetic(result)
 
 message = "one time pad"
 
+
 message = message.upper()
 message = message.replace(" ", "")
-
+print(f"original:  '{message}'")
 x = encrypt(message, key)
 
-print(x)
-
+print(f"encrypted: '{x}'")
+print(type(x))
 x = decrypt(x, key)
 
-print(x)
+print(f"decrypted: '{x}'")
 
